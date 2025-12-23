f5649f80540b0b7e9f5e8252442b6212f2c52602
Davron <uzmacbook@MacBookPro.lan>
2025-12-23 09:37:41 +0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClientProvider } from "react-query";
 import Router from "./router/index";
 import { queryClient } from "./service/query-client/index";
 import { ToastProvider } from "./utils/toast/ToastContainer";
-import "aos/dist/aos.css";
 import "./i18n";
 
 function App() {
